f6264e1f402879257e262a5f45c66e7e50877286
bot <contact@arca-computing.fr>
2016-05-22 19:43:22 +0200

    Add new config

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 -- Wifi
-AP = ""
-PWD = ""
+AP = "WIFWIFI"
+PWD = "192Wifsimster!!"
 
 -- MQTT Broker
-BROKER_IP = "192.168.0.X"
+BROKER_IP = "192.168.0.35"
 BROKER_PORT = 1883
 CLIENT_ID = "ESP8266-"..node.chipid()
 REFRESH_RATE = 30000 -- ms
